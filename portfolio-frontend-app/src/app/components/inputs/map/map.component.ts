d37afdf1dea7a7bb2e46e6074a88b7b00cdcc025
HandreiFontana <handreifontana@gmail.com>
2023-05-08 17:47:01 -0300

    fix [frontend]: created a list of layers that was not bringing the layer from outside the class

--- portfolio-frontend-app/src/app/components/inputs/map/map.component.ts
+++ portfolio-frontend-app/src/app/components/inputs/map/map.component.ts
@@ -3,11 +3,6 @@ import { ControlValueAccessor, FormControlName, NG_VALUE_ACCESSOR } from '@angul
 import * as L from 'leaflet'
 import { Map, Marker, Polygon, TileLayer } from 'leaflet'
 
-const defaultLayer: TileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '© OpenStreetMap'
-})
-
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -26,11 +21,20 @@ export class MapComponent implements OnInit, ControlValueAccessor {
   private marker: Marker
   private polygon: Polygon
 
+  private tileLayers: TileLayer[] = [
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    })
+  ]
+  private defaultTileLayer = this.tileLayers[0]
+
   public value: any
   private onChange: Function
 
   @Input() formControlName?: FormControlName
   @Input() label?: string
+  @Input() isDisabled?: boolean = false
   @Input() dataType?: 'marker' | 'polygon'
 
   constructor() { }
@@ -43,6 +47,7 @@ export class MapComponent implements OnInit, ControlValueAccessor {
 
   writeValue(value: any): void {
     this.value = value
+    if (value) this.centerMap(this.dataType, value)
   }
 
   registerOnChange(fn: any): void {
@@ -69,8 +74,21 @@ export class MapComponent implements OnInit, ControlValueAccessor {
 
   private addTileLayer(layer?: TileLayer) {
     this.tileLayer?.remove()
+    
+    this.tileLayer = (layer ?? this.defaultTileLayer).addTo(this.map)
+  }
 
-    this.tileLayer = (layer ?? defaultLayer).addTo(this.map)
+  private centerMap(type: string, value: any) {
+    switch (type) {
+      case 'marker':
+        this.map.setView({ lat: value[0], lng: value[1] })
+        this.markerAdd(value[0], value[1])
+        break
+      case 'polygon':
+        this.map.fitBounds(value)
+        this.polygonAddToMap(value)
+        break
+    }
   }
 
   // Map Click Functions
@@ -78,13 +96,15 @@ export class MapComponent implements OnInit, ControlValueAccessor {
   private onMapClick(type: string, event: any): void {
     const { lat, lng } = event.latlng
 
-    switch (type) {
-      case 'marker':
-        this.markerAdd(lat, lng)
-        break
-      case 'polygon':
-        this.polygonAdd(lat, lng)
-        break
+    if (!this.isDisabled) {
+      switch (type) {
+        case 'marker':
+          this.markerAdd(lat, lng)
+          break
+        case 'polygon':
+          this.polygonAdd(lat, lng)
+          break
+      }
     }
   }
 
@@ -101,12 +121,16 @@ export class MapComponent implements OnInit, ControlValueAccessor {
     this.value = this.value ? [...this.value, [lat, lng]] : [[lat, lng]]
 
     if (this.value.length > 2) {
-      this.onChange(this.value)
-
-      this.polygon?.remove()
-
-      this.polygon = L.polygon(this.value).addTo(this.map)
+      this.polygonAddToMap(this.value)
     }
+  }
+
+  private polygonAddToMap(latlng: L.LatLngExpression[]) {
+    this.onChange(latlng)
+
+    this.polygon?.remove()
+
+    this.polygon = L.polygon(latlng).addTo(this.map)
   }
 
   public polygonRemove(): void {
